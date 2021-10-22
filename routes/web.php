f626544ae16e624f3a32649c449f36086248022b
Asyraf Rahim <asyrafboyz@gmail.com>
2021-10-22 11:43:41 +0800

    Generate Sitemap XML

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\SitemapXmlController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +19,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/', [BlogController::class, 'index']);
+Route::get('/sitemap.xml', [SitemapXmlController::class, 'index']);
